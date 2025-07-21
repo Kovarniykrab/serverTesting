9ec43111e8dea01c6c238e870a329af26597e88e
Kovarniykrab <Zaratos1999@gmail.com>
2025-07-21 14:43:54 +0700

    добавил структуру User и фуркцию ToUser

--- myApp/domain/user.go
+++ myApp/domain/user.go
@@ -1,11 +1,19 @@
 package domain
 
+type User struct {
+	ID           int    `bun:"id" json:"id"`
+	Email        string `bun:"email" json:"email"`
+	Login        string `bun:"login" json:"login"`
+	PasswordHash string `bun:"password_hash" json:"-"`
+	PhoneNumber  string `bun:"phone" json:"phone"`
+}
+
 type RegisterUser struct {
-	Email           string `bun:"email" json:"email"`
-	Login           string `bun:"login" json:"login"`
-	PasswordHash    string `bun:"password_hash" json:"password"`
-	ConfirmPassword string `json:"confirm_password" validate:"required,eqfield=PasswordHash"`
-	PhoneNumber     string `bun:"phone" json:"phone"`
+	Email           string `json:"email"`
+	Login           string `json:"login"`
+	PasswordHash    string `json:"password"`
+	ConfirmPassword string `validate:"required,eqfield=PasswordHash"`
+	PhoneNumber     string `json:"phone"`
 }
 
 type UserAuth struct {
@@ -14,6 +22,16 @@ type UserAuth struct {
 }
 
 type ChangePass struct {
+	OldPassword string `json:"old_password" validate:"required"`
 	Password    string `json:"password"`
 	ConfirmPass string `json:"confirm_pass" validate:"required,eqfield=Password"`
+}
+
+func (r *RegisterUser) toUser() *User {
+	return &User{
+		Email:        r.Email,
+		Login:        r.Login,
+		PasswordHash: r.PasswordHash,
+		PhoneNumber:  r.PhoneNumber,
+	}
 }
