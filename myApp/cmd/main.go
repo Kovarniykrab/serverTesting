d5a45f74c383a910f0a59755da7e212f2b45d995
Kovarniykrab <Zaratos1999@gmail.com>
2025-08-11 22:25:28 +0700

    сваг

--- myApp/cmd/main.go
+++ myApp/cmd/main.go
@@ -1,18 +1,36 @@
-package cmd
+package main
 
 import (
-	"github.com/Kovarniykrab/serverTesting/myApp/api/handlers"
+	"fmt"
+
+	"github.com/Kovarniykrab/serverTesting/api/handlers"
+	"github.com/Kovarniykrab/serverTesting/api/routers"
+	_ "github.com/Kovarniykrab/serverTesting/docs"
+	"github.com/fasthttp/router"
+	swagger "github.com/swaggo/fasthttp-swagger"
+	"github.com/valyala/fasthttp"
 )
 
 // @title          TestUser API
 // @version        0.5
 // @description    API для управления пользователями
-// @host           localhost:8080
-// @BasePath       /api
+// @host           localhost:8085
+// @BasePath       /swagger/
 // @securityDefinitions.apikey  ApiKeyAuth
 // @in                          header
 // @name                        Authorization
 
 func main() {
 	var _ = handlers.RegisterUserHandler
+	fmt.Println("API server started on :8080")
+	go func() {
+		r := routers.GetRouter()
+		fasthttp.ListenAndServe(":8080", r.Handler)
+	}()
+
+	fmt.Println("Swagger server started on :8085")
+	swaggerRouter := router.New()
+
+	swaggerRouter.GET("/swagger/*any", swagger.WrapHandler())
+	fasthttp.ListenAndServe(":8085", swaggerRouter.Handler)
 }
